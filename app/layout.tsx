6667f961a652c788756938c4f6e880b99ad81090
Mohammad Mansour <abdultawab940@gmail.com>
2024-09-07 08:21:29 +0300

    feat: improved SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,57 +5,56 @@ import { getLang } from "@/utils/getLang";
 import { cookies } from "next/headers";
 import { getDictionary } from "./dictionaries";
 
-export async function generateMetadata(): Promise<Metadata> {
-	const lang = getLang(cookies());
-	const dict = await getDictionary(lang);
-
-	return {
+export const metadata: Metadata = {
+	title: "بسيط - Basset",
+	twitter: {
+		images: "https://i.imgur.com/NLbi3nc.png",
 		title: "بسيط - Basset",
-		description: dict.description,
-		twitter: {
-			images: "https://i.imgur.com/NLbi3nc.png",
-			title: "بسيط - Basset",
-			description: dict.description,
-		},
-		openGraph: {
-			images: "https://i.imgur.com/NLbi3nc.png",
-			title: "بسيط - Basset",
-			description: dict.description,
-		},
-		keywords: [
-			"convert",
-			"cut",
-			"trim",
-			"compress",
-			"quality change",
-			"ffmpeg",
-			"ffmbeg",
-			"ffmpeg gui",
-			"video to audio",
-			"audio to video",
-			"Basset",
-			"basset",
-			"Video edit",
-			"بسيط",
-			"تعديل مقاطع",
-			"تحويل صيغة",
-			"قص مقاطع",
-			"قص صوتيات",
-			"ضغط مقاطع",
-			"ضغط صوتيات",
-			"تحويل فيديو",
-			"تحويل مقطع",
-			"تحويل صوتية",
-		],
-	};
-}
+	},
+	openGraph: {
+		images: "https://i.imgur.com/NLbi3nc.png",
+		title: "بسيط - Basset",
+	},
+	keywords: [
+		"convert",
+		"cut",
+		"trim",
+		"compress",
+		"quality change",
+		"ffmpeg",
+		"ffmbeg",
+		"ffmpeg gui",
+		"video to audio",
+		"audio to video",
+		"Basset",
+		"basset",
+		"Video edit",
+		"بسيط",
+		"تعديل مقاطع",
+		"تحويل صيغة",
+		"قص مقاطع",
+		"قص صوتيات",
+		"ضغط مقاطع",
+		"ضغط صوتيات",
+		"تحويل فيديو",
+		"تحويل مقطع",
+		"تحويل صوتية",
+	],
+};
 
-export default function RootLayout({
+export default async function RootLayout({
 	children,
 }: Readonly<{
 	children: React.ReactNode;
 }>) {
 	const lang = getLang(cookies());
+	const dict = await getDictionary(lang);
+
+	metadata.description = dict.description;
+	// biome-ignore lint/style/noNonNullAssertion: They are not null
+	metadata!.openGraph!.description! = dict.description;
+	// biome-ignore lint/style/noNonNullAssertion: They are not null
+	metadata!.twitter!.description! = dict.description;
 
 	return (
 		<html dir={dir(lang)} lang={lang}>
